d7092b9f693be81195e639c047ddd06096c99803
joneschrisan <joneschrisan@aol.com>
2012-04-05 22:53:21 +0200

    Updated to version 0.2.1 Changed the UI bit that it writes into the poke page to be more user friendly

--- Facebook-AutoPoke.js
+++ Facebook-AutoPoke.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       Facebook AutoPoke
-// @namespace  https://joneschrisan@github.com/joneschrisan/Facebook-AutoPoke
-// @version    0.2
+// @namespace  https://github.com/joneschrisan/Facebook-AutoPoke
+// @version    0.2.1
 // @description  Atomaticaly pokes people who poke you on facebook
 // @match      http://www.facebook.com/pokes
 // @copyright  2012+, Chris 'CJ' Jones
@@ -9,29 +9,33 @@
 
 function do_poke() {
     if(AutoPokeCounterOnOff.checked == true) {
-        AutoPokePasses++;
         var AutoPokeATags = document.getElementsByTagName("a");
-        //alert(atags);
         for(i = 0; i < AutoPokeATags.length; i++) {
-            //alert(atags[i].innerHTML);
             if(AutoPokeATags[i].innerHTML.match(/<img[a-z|A-Z|:|\/|=|\"|0-9|_| |.|;|]*>Poke back/gi)) {
                 var evt = document.createEvent("MouseEvents");
                 evt.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
                 var canceled = !AutoPokeATags[i].dispatchEvent(evt);
-                if(canceled) {
-                  //alert("canceled");
-                } else {
-                  //alert("not canceled");
+            }
+        }
+        if(document.getElementById("AutoPokeDeletePokesOnOff").checked == true) {
+            var AutoPokeCloseButtons = pagelet_pokes.getElementsByTagName("a");
+            for(i = 0; i < AutoPokeCloseButtons.length; i++) {
+                if(AutoPokeCloseButtons[i].title == "Remove") {
+                    var evt2 = document.createEvent("MouseEvents");
+                    evt2.initMouseEvent("click", true, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+                    var canceled = !AutoPokeCloseButtons[i].dispatchEvent(evt2);
+                    AutoPokeNumPokes++;
+                    var AutoPokePokesCounter = document.getElementById("AutoPokePokes");
+                    AutoPokePokesCounter.innerHTML = AutoPokeNumPokes;
                 }
             }
         }
-        AutoPokeCounter.innerHTML = AutoPokePasses;
     }
 }
 
 function start_AutoPoke() {
-    //alert("Facebook AutoPoke started");
     AutoPokePasses = 0;
+    AutoPokeNumPokes = 0;
     
     AutoPokeH2Tags = document.getElementsByTagName("h2");
     for(i = 0; i < AutoPokeH2Tags.length; i++) {
@@ -42,14 +46,25 @@ function start_AutoPoke() {
     
     var AutoPokeContainer = document.createElement("h3");
     
-    var AutoPokePassesText = document.createElement("span")
-    AutoPokePassesText.innerHTML = "AutoPoke Passes: ";
+    var AutoPokePokesText = document.createElement("span");
+    AutoPokePokesText.innerHTML = "Pokes back: ";
     
-    var AutoPokeCounter = document.createElement("span");
-    AutoPokeCounter.id = "AutoPokeCounter";
-    AutoPokeCounter.innerHTML = "0";
+    var AutoPokePokes = document.createElement("span");
+    AutoPokePokes.type = "text";
+    AutoPokePokes.id = "AutoPokePokes";
+    AutoPokePokes.innerHTML = "0";
     
     var AutoPokeBR = document.createElement("br");
+    
+    var AutoPokeDeletePokesText = document.createElement("span");
+    AutoPokeDeletePokesText.innerHTML = "Delete pokes after poke back: ";
+    
+    var AutoPokeDeletePokesOnOff = document.createElement("input");
+    AutoPokeDeletePokesOnOff.type = "checkbox";
+    AutoPokeDeletePokesOnOff.id = "AutoPokeDeletePokesOnOff";
+    AutoPokeDeletePokesOnOff.checked = "checked";
+    
+    var AutoPokeBR2 = document.createElement("br");
     
     var AutoPokeOnOffText = document.createElement("span");
     AutoPokeOnOffText.innerHTML = "AutoPoke On: ";
@@ -58,20 +73,29 @@ function start_AutoPoke() {
     AutoPokeCounterOnOff.type = "checkbox";
     AutoPokeCounterOnOff.id = "AutoPokeCounterOnOff";
     
-    AutoPokeContainer.appendChild(AutoPokePassesText);
-    AutoPokeContainer.appendChild(AutoPokeCounter);
+    AutoPokeContainer.appendChild(AutoPokePokesText);
+    AutoPokeContainer.appendChild(AutoPokePokes);
     AutoPokeContainer.appendChild(AutoPokeBR);
+    AutoPokeContainer.appendChild(AutoPokeDeletePokesText);
+    AutoPokeContainer.appendChild(AutoPokeDeletePokesOnOff);
+    AutoPokeContainer.appendChild(AutoPokeBR2);
     AutoPokeContainer.appendChild(AutoPokeOnOffText);
     AutoPokeContainer.appendChild(AutoPokeCounterOnOff);
     
     AutoPokeTitle.appendChild(AutoPokeContainer);
     
-    var AutoPokeTimeOutId = self.setInterval(do_poke, 5000);
+    AutoPokeTimeOutId = self.setInterval(do_poke, 5000);
 }
 
+var AutoPokeTimeOutId;
+
 var AutoPokePasses;
+var AutoPokeNumPokes;
 var AutoPokeH2Tags;
-    
+
+var pagelet_pokes;
+
 window.onload = function() {
+    pagelet_pokes = document.getElementById("pagelet_pokes");
     start_AutoPoke()
 }
